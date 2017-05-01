56dea8d943aaa57090c41d057e2ec1d9232a4f87
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-05-01 13:48:27 -0700

    Added row classes for styling and organization

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Examples = React.createClass({
 // 	render: function () {
 // 		return (
@@ -12,8 +12,16 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
-			<h2>Examples</h2>
-			<h3>Welcome to the examples page</h3>
+			<h1 className='text-center'>Examples</h1>
+			<p>Here are a few example locations to try out</p>
+				<ol>
+					<li>
+						<Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+					</li>
+					<li>
+						<Link to='/?location=Rio?'>Rio, Brazil</Link>
+					</li>
+				</ol>
 		</div>
 	)
 };
