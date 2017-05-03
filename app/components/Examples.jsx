332490a193d17cdba38c69062231d6a147caa364
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-05-02 17:11:05 -0700

    Add nav search support

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -19,7 +19,10 @@ var Examples = (props) => {
 						<Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
 					</li>
 					<li>
-						<Link to='/?location=Rio?'>Rio, Brazil</Link>
+						<Link to='/?location=Rio'>Rio, Brazil</Link>
+					</li>
+					<li>
+						<Link to='/?location=Bogota'>Bogota, Colombia</Link>
 					</li>
 				</ol>
 		</div>
